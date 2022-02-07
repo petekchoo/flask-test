bdb735ce025225e0ba8223a0397ee0707fb66db3
PC <pkchoo@gmail.com>
2022-02-06 18:39:02 -0700

    basic form

--- application.py
+++ application.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 application = Flask(__name__)
 
-@application.route('/')
-
-def hello_world():
-    strUserInput = input("Input word!")
-    
-    return strUserInput
+@application.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@application.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+ 
+ 
+application.run(host='localhost', port=5000)
