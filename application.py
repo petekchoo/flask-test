1a0473cc49bca6cd515e6aba04610c7123f256e6
PC <pkchoo@gmail.com>
2022-02-07 08:06:25 -0700

    consolidated form page

--- application.py
+++ application.py
@@ -9,10 +9,10 @@ def welcome():
 def form():
     return render_template('/form.html')
  
-@application.route('/data/', methods = ['POST', 'GET'])
+@application.route('/data', methods = ['POST', 'GET'])
 def data():
     if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+        return "The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
-        form_data = request.form
-        return render_template('/data.html',form_data = form_data)
+        dataForm = request.form
+        return render_template('/data.html',form_data = dataForm)
