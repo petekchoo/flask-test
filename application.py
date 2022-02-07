ee69c2cce09e6d4b7c8f697e236098d28b46145d
PC <pkchoo@gmail.com>
2022-02-07 15:02:30 -0700

    updated shared homepage

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 application = Flask(__name__)
 
-@application.route('/')
+@application.route('/', methods = ['POST', 'GET'])
 def welcome():
-    return render_template('index.html')
+    return render_template('index.html', variableA = 0, variableB = 1)
 
 @application.route('/form')
 def form():
@@ -15,4 +15,12 @@ def data():
         return "The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
         dataForm = request.form
-        return render_template('/data.html',form_data = dataForm)
+        return render_template('/data.html', form_data = dataForm)
+
+def manipulateInputs(inputA, inputB):
+    strTempA = inputA
+    strTempB = inputB
+
+    return strTempB, strTempA
+
+application.run()
