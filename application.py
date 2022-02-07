83fd41ace36fef4ba62a2493df931e790cb5ab92
PC <pkchoo@gmail.com>
2022-02-06 19:01:29 -0700

    fix

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
 application = Flask(__name__)
 
+@application.route('/')
+def welcome():
+    return 'hello! welcome to this webapp', '\n', render_template('index.html')
+
 @application.route('/form')
 def form():
-    return render_template('form.html')
+    return render_template('/form.html')
  
 @application.route('/data/', methods = ['POST', 'GET'])
 def data():
@@ -11,7 +15,6 @@ def data():
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
         form_data = request.form
-        return render_template('data.html',form_data = form_data)
- 
- 
-application.run(host='localhost', port=5000)
+        return render_template('/data.html',form_data = form_data) 
+
+application.run()
