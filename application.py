923b41e3d5beda5d9ac5cd5da885f95f40ecf096
PC <pkchoo@gmail.com>
2022-02-14 20:52:58 -0500

    radical wordle update

--- application.py
+++ application.py
@@ -1,24 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for
 application = Flask(__name__)
 
-@application.route('/', methods = ['POST', 'GET'])
-def welcome():
-    return render_template('index.html', variableA = 0, variableB = 1)
+@application.route('/')
+def homepage():
+    return render_template('index.html')
 
-@application.route('/form')
-def form():
-    return render_template('/form.html')
- 
-@application.route('/data', methods = ['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return "The URL /data is accessed directly. Try going to '/form' to submit form"
+# allow both GET and POST requests
+@application.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    # handle the POST request
+    greenlist = []
+    greylist = []
+    yellowlist = [[], [], [], [], []]
+    bestword = ""
+     
     if request.method == 'POST':
-        dataForm = request.form
-        return render_template('/data.html', form_data = dataForm)
+        
+        for i in range(1, 6):
+            greenlist.append(request.form.get('l'+str(i)+'g'))
 
-def manipulateInputs(inputA, inputB):
-    strTempA = inputA
-    strTempB = inputB
+        for letter in request.form.get('grey'):
+            greylist.append(letter)
 
-    return strTempB, strTempA
+        for i in range(1, 6):
+            for letter in request.form.get('l'+str(i)+'y'):
+                yellowlist[i-1].append(letter)
+
+        return render_template('form.html', green = greenlist, grey = greylist, yellow = yellowlist, word = bestword)
+
+    # otherwise handle the GET request
+    return '''
+           <form method="POST">
+    <div><label>Letter 1 Green: <input type="text" name="l1g"></label></div>
+    <div><label>Letter 2 Green: <input type="text" name="l2g"></label></div>
+    <div><label>Letter 3 Green: <input type="text" name="l3g"></label></div>
+    <div><label>Letter 4 Green: <input type="text" name="l4g"></label></div>
+    <div><label>Letter 5 Green: <input type="text" name="l5g"></label></div>
+    <div><label>Letter 1 Yellows: <input type="text" name="l1y"></label></div>
+    <div><label>Letter 2 Yellows: <input type="text" name="l2y"></label></div>
+    <div><label>Letter 3 Yellows: <input type="text" name="l3y"></label></div>
+    <div><label>Letter 4 Yellows: <input type="text" name="l4y"></label></div>
+    <div><label>Letter 5 Yellows: <input type="text" name="l5y"></label></div>
+    <div><label>Grey Letters: <input type="text" name="grey"></label></div>
+    <input type="submit" value="Submit"></form>'''
+
+application.run()
